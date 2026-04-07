7f02b74fc744d0f6eaec4b4c59c8a9e108a89b2c
Raxon Suri <raxonsuriperez@gmail.com>
2026-04-07 19:06:46 -0400

    Main Panel styles
    Done:
    Styles
    
    Working on:
    Implementing localStorage.
    Implementing drag-and-drop with Swapy.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useReducer } from "react";
 import { useForm } from "./helpers/useForm";
+import './styles/App.css'
 
 const reducer = (state, action = {}) => {
   switch(action.type){
@@ -45,34 +46,61 @@ export const App = () => {
 
   return (
     <>
-      <h1>Main panel</h1>
-  
-      <div id="task-list">
-        <form onSubmit={addTask}>
-          <label htmlFor="adding-tasks">Add task</label>  
-          <input type="text" value={task} onChange={onInputChange} name="task" placeholder="Wash dishes" required  className="border"/>
+      <header className="bg-[#294C60] border border-[#467c9b] w-screen p-7 rounded-b-xl">
+        <h1 className="text-center text-3xl underline">Main Dashboard</h1>
+      </header>
+    
+      <div id="task-list" className="flex justify-center my-10">
+        <form onSubmit={addTask} className="bg-[#294C60] border border-[#467c9b] rounded-md px-4 py-3 pb-5 w-84">
+          <div className="text-center">
+            <label htmlFor="adding-tasks" className="text-xl underline">Add task</label>  
+          </div>
+          
+          <div className="mt-2">
+            <input type="text" value={task} onChange={onInputChange} name="task" placeholder="Wash dishes" required  className="border rounded-md py-1 px-2 text-xl"/>
 
-          <button className="text-black">
-            <i className="fa-solid fa-arrow-up"></i>
-          </button>
+            <button className="border rounded-md px-2 py-1 ml-2 text-xl">
+              <i className="fa-solid fa-arrow-up"></i>
+            </button>
+          </div>
         </form>
       </div>
 
-      <div>
-          <ul>
-          {newState.map(task => {
-            return(
-              <li key={task.id}>
-                {task.taskName}
+      <div className="flex justify-center mt-4">
+        <div className="bg-[#294C60] justify-center border border-[#467c9b] rounded-md w-[32%] p-3 ml-2">
+          <h2 className="text-2xl text-center mb-2 underline">To-Do</h2>
+          
+          <ul className="w-[90%]">
+            {newState.map(task => {
+              return(
+                <li key={task.id} className="text-2xl flex gap-2 my-2">
+                  <p>{task.taskName}</p>
 
-                <button onClick={() => deleteTask(task)}>
-                  <i className="fa-solid fa-trash"></i>
-                </button>
-              </li>
-            )
-          })}  
+                  <button onClick={() => deleteTask(task)} className="text-xl">
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
+                </li>
+              )
+            })}  
           </ul>
         </div>
+
+        <div className="bg-[#294C60] flex justify-center border border-[#467c9b] rounded-md w-[32%] p-3 mx-3">
+          <h2 className="text-2xl text-center mb-2 underline">On Process</h2>
+        </div>
+
+
+        <div className="bg-[#294C60] flex justify-center border border-[#467c9b] rounded-md w-[32%] p-3">
+          <h2 className="text-2xl text-center mb-2 underline">Done</h2>
+        </div>
+      </div>
     </>
   )
 }
+
+/*
+  BG - #001B2E
+  DIV BG - #294C60 
+  DIV BORDER - #467c9b
+  TEXT - #FFEFD3
+*/
